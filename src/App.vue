5e144c3219cfc45d746a4eade2042e687d81bc25
ricardocastanho <ricardinhorl@hotmail.com>
2020-06-18 17:18:17 -0300

    first day

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-dark bg-dark">
+     <a class="navbar-brand btn " href="#"><h3 class="text-danger text-left">MoviesFlix</h3></a>
+     <a class="navbar-brand btn btn-sm btn-danger" href="#"><h3 class="text-ligth text-right">Login</h3></a>
+    </nav>
+    <div class="card bg-dark text-center text-light">
+      <div class="card-body">
+          <br><br><br>
+          <h1>Milhares de filmes e séries.</h1>
+          <h1>Diversão garantida.</h1>
+          <h5>Você vai adorar a Netflix! Mas, se não gostar, cancele</h5>
+          <h5>nos primeiros 21 dias para receber o reembolso.</h5>
+          <br>
+          <form action="">
+            <input placeholder="Digite seu Email" type="text">
+            <button type="submit" class="btn btn-danger">VAMOS LÁ</button>
+          </form>
+          &nbsp;
+          <br><br>
+          <h6>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</h6>
+          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+          <br><br><br><br><br><br>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
