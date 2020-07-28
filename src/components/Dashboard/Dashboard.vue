ac4f9137e38d7337dd436c1d36a77c71e5df3a8a
ricardocastanho <ricardinhorl@hotmail.com>
2020-07-28 16:04:21 -0300

    Layout do dashboard mais top

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,71 +1,99 @@
 <template>
-    <div>
-        <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand btn " href="#"><h3 class="text-danger text-left">MoviesFlix</h3></a>
-        <button v-on:click="logout()" class="navbar-brand btn btn-sm btn-secondary">Logout</button>
-        </nav>
-        <div class="card bg-dark text-center text-light">
-        <div class="card-body">
-            <br><br><br>
-            <div class="container">
-                <div class="card-group">
-                    <div v-for="m in movie" v-bind:key="m.id"  class="card bg-dark">
-                        <div class="card-body bg-dark">
-                            <p>{{ m.name }}</p>
-                            {{ m.description }}
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand btn" href="#">
+        <h3 class="text-danger text-left">MoviesFlix</h3>
+      </a>
+      <button v-on:click="logout()" class="navbar-brand btn btn-sm btn-dark">Sair</button>
+    </nav>
+    <div class="card bg-dark text-center text-light">
+      <div class="card bg-dark img-fundo" style="padding-top: 100px;padding-bottom: 100px">
+        <div class="card-body text-left">
+          <h1>
+            <b>{{ movie[0].name }}</b>
+          </h1>
+          <h6
+            style="width: 500px;text-align: justify;text-justify: inter-word;"
+          >{{ movie[0].description }}</h6>
+          <br />
+          <button class="btn btn-lg btn-dark">
+            <span class="fa fa-play"></span> Assistir
+          </button>
+          &nbsp;
+          <button class="btn btn-lg btn-dark">
+            <span class="fa fa-plus"></span> Lista
+          </button>
+        </div>
+      </div>
+
+      <div class="card-body">
+        <div class="card bg-dark">
+          <div class="card-body text-left">
+            <h5>
+              <b>Mais assistidos da semana:</b>
+            </h5>
+            <div class="carousel-item">
+              <img url="./" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>...</h5>
+                <p>...</p>
+              </div>
             </div>
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-            <br><br><br><br><br><br>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios/dist/axios"
+import axios from 'axios/dist/axios'
+import 'font-awesome/css/font-awesome.css'
 
 export default {
-    data(){
-        return{
-            movie: [{
-                name: "",
-                description: ""
-            }]
-        }
-    },
-    methods: {
-        getMovies(){
-            axios({
-                url: "http://localhost:7542",
-                method: "post",
-                data: {
-                    query: `{
-                        movies{ name description }
-                    }`
-                }, headers: {
-                    authorization: localStorage.getItem('token')
-                }
-            }).then(resp => {
-                if(resp.data.errors == undefined){
-                    this.movie = resp.data.data.movies
-                }else{
-                    this.$router.push('/login')
-                }
-            }).catch(err => {
-                console.log(err.data.errors)
-            })
+  data() {
+    return {
+      movie: [
+        {
+          name: '',
+          description: '',
         },
-        logout(){
-            localStorage.removeItem('token')
-            this.$router.push('/login')
-        }
-    },
-    
-    created() {
-        this.getMovies()
+      ],
     }
+  },
+  methods: {
+    getMovies() {
+      axios({
+        url: 'http://localhost:7542',
+        method: 'post',
+        data: {
+          query: `{
+            movies{ name description }
+          }`,
+        },
+        headers: {
+          authorization: localStorage.getItem('token'),
+        },
+      })
+        .then((resp) => {
+          if (resp.data.errors == undefined) {
+            this.movie = resp.data.data.movies
+          } else {
+            this.$router.push('/login')
+          }
+        })
+        .catch((err) => {
+          console.log(err.data.errors)
+        })
+    },
+    logout() {
+      localStorage.removeItem('token')
+      this.$router.push('/login')
+    },
+  },
+
+  created() {
+    this.getMovies()
+  },
 }
 </script>
--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,59 +1,66 @@
 <template>
-    <div>
-        <nav class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand btn " href="/"><h3 class="text-danger text-left">MoviesFlix</h3></a>
-        </nav>
-        <div class="card bg-dark text-center text-light">
-            <div class="card-body">
-                <br><br><br>
-                <h1>Entrar</h1>
-                <input id="email" placeholder="Digite seu Email" type="text"><br><br>
-                <input id="password" placeholder="Senha" type="password"><br>
-                <div id="alert" class="alert alert-danger col-3" role="alert" style="display: none">
-                    {{ alert }}
-                </div>
-                <br>
-                <button v-on:click='login()' type="submit" class="btn btn-danger">Entrar</button>
-                &nbsp;
-                <br><br>
-                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br>
-            </div>
-        </div>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand btn" href="/">
+        <h3 class="text-danger text-left">MoviesFlix</h3>
+      </a>
+    </nav>
+    <div class="card bg-dark text-center text-light">
+      <div class="card-body">
+        <br />
+        <br />
+        <br />
+        <h1>Entrar</h1>
+        <input id="email" placeholder="Digite seu Email" type="text" />
+        <br />
+        <br />
+        <input id="password" placeholder="Senha" type="password" />
+        <br />
+        <div
+          id="alert"
+          class="alert alert-danger col-3"
+          role="alert"
+          style="display: none"
+        >{{ alert }}</div>
+        <br />
+        <button v-on:click="login()" type="submit" class="btn btn-danger">Entrar</button>
+        &nbsp;
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios/dist/axios"
 
 export default {
-    name: "App",
-    data() {
-        return {
-            alert: ""
-        }
-    },
-    methods: {
-        login(){
-            let email = document.getElementById("email").value
-            let password = document.getElementById("password").value
-            axios({
-                url: "http://localhost:7542",
-                method: "post",
-                data: {
-                    query: `{ login(data: {
+  name: "App",
+  data() {
+    return {
+      alert: ""
+    }
+  },
+  methods: {
+    login() {
+      let email = document.getElementById("email").value
+      let password = document.getElementById("password").value
+      axios({
+        url: "http://localhost:7542",
+        method: "post",
+        data: {
+          query: `{ login(data: {
                                 email: "${email}"
                                 password: "${password}"
                             }){ name email token } }`
-                }
-            }).then(result => {
-                localStorage.setItem('token', result.data.data.login.token)
-                this.$router.push('/dashboard')
-            }).catch(errors => {
-                document.getElementById("alert").attributes.style.value = "margin:15px auto"
-                this.alert = errors.message
-            })
         }
+      }).then(result => {
+        localStorage.setItem('token', result.data.data.login.token)
+        this.$router.push('/dashboard')
+      }).catch(errors => {
+        document.getElementById("alert").attributes.style.value = "margin:15px auto"
+        this.alert = errors.message
+      })
     }
+  }
 }
 </script>
